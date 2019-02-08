8004ad8d7724d7a6e39e48bb40debe9c3cbcffc7
sowjanya <sowjanyadarna@gmail.com>
2019-02-08 20:32:31 +0530

    ass4_7 added

---
+++ Project5/Project5/ass4_7.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	char *str[10];
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		str[i] = (char*)malloc(sizeof(char));
+	}
+	for (i = 0; i < 10; i++)
+		gets(str[i]);
+		//scanf_s("%s", str[i], sizeof(str[i]));
+	for (i = 0; i < 10; i++)
+	{
+		printf("%s\n", str[i]);
+	}
+	getchar();
+}
